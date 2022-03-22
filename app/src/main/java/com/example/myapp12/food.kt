a3b0ca1f9fa5773aa512d5f5d3a7c37f23c3c0b3
AhmedSayedMhvouz <ahmed.sayed.mhvouz.abdelhameed@gmail.com>
2022-03-22 18:25:14 +0200

    g

---
+++ app/src/main/java/com/example/myapp12/food.kt
@@ -0,0 +1,11 @@
+package com.example.myapp12
+
+class  Food {
+    var name:String? =null
+    var des:String? =null
+    var image:Int? =null
+    constructor(name:String,des:String,image:Int){
+        this.name=name
+        this.des=des
+        this.image=image
+    }}
